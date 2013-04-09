954f0855154d54595039df723c519c7003c30a9b
assertivist <croc+github@sdf.org>
2013-04-09 00:08:07 -0500

    fixed bug where pavara tries to load .DS_Store files as levels

--- map_test.py
+++ map_test.py
@@ -58,6 +58,8 @@ class Map_Test (ShowBase):
         for idx,item in enumerate(os.listdir('Maps')):
             if idx == 0:
                 self.switch_map(item)
+            if item[0] == ".":
+                continue
             item_div = self.doc.CreateElement("div")
             item_div.SetAttribute("map", item)
             item_div.AddEventListener('click', self.map_selected, True)
